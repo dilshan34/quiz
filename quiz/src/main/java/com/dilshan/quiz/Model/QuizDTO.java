4f168aa8f9dc0a427ce75b52a6c17670c70732b4
dilshan34 <dilshansachinthana34@gmail.com>
2025-04-18 15:36:50 +0530

    add eureka

---
+++ quiz/src/main/java/com/dilshan/quiz/Model/QuizDTO.java
@@ -0,0 +1,40 @@
+package com.dilshan.quiz.Model;
+
+public class QuizDTO {
+
+    private String category;
+    private int noOfQuestion;
+    private String title;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getNoOfQuestion() {
+        return noOfQuestion;
+    }
+
+    public void setNoOfQuestion(int noOfQuestion) {
+        this.noOfQuestion = noOfQuestion;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public QuizDTO(String category, int noOfQuestion, String title) {
+        this.category = category;
+        this.noOfQuestion = noOfQuestion;
+        this.title = title;
+    }
+
+    QuizDTO(){}
+}
